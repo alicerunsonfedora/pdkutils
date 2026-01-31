893f0e3c9a17d1e693568e416e4090a46f17ea09
Marquis Kurt <software@marquiskurt.net>
2026-01-31 00:42:42 -0500

    Skip overview conversion

--- Scripts/docsgen.sh
+++ Scripts/docsgen.sh
@@ -17,11 +17,9 @@ echo "Copying symbols..."
 cp -r /tmp/derivedData/Build/Intermediates.noindex/*.build/Release/*.build/symbol-graph/ /tmp/symbols
 rm -rf /tmp/symbols/swift* /tmp/symbols/playdatekit /tmp/symbols/utf8viewextensions
 
-xcrun docc convert "./PDKUtils.docc" \
-  --output-dir /tmp/docc/PDKUtils_Glue.doccarchive \
-  --additional-symbol-graph-dir /tmp/symbols
-
 xcrun docc merge /tmp/docc/PDKUtils.doccarchive $(find /tmp/derivedData -type d -name 'PD*.doccarchive') \
+  --synthesized-landing-page-name PDKUtils \
+  --synthesized-landing-page-kind SDK \
   --output-path /tmp/docc/PDKUtils.doccarchive
 
 $(xcrun -f docc -toolchain "swift latest") process-archive \
